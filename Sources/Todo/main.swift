2393aff97d38bae87c1de288ba70d71e0349726d
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-11 01:09:07 -0200

    compose routers

--- Sources/Todo/main.swift
+++ Sources/Todo/main.swift
@@ -16,28 +16,38 @@ import Venice
 import CHTTPParser
 import CLibvenice
 
-let router = Router("/api/v1") { route in
+OpenSSL.initialize()
+
+let todo = TodoResources()
+
+let v1 = Router("/v1") { route in
     route.get("/version") { _ in
         return Response(status: .OK, json: ["version": "1.0.0"])
     }
+}
 
-    let todo = TodoResources()
+let v2 = Router("/v2") { route in
+    route.get("/version") { _ in
+        return Response(status: .OK, json: ["version": "2.0.0"])
+    }
+
     route.get("/todos", todo.index)
     route.post("/todos", parseJSON >>> todo.create)
     route.get("/todos/:id", todo.show)
     route.put("/todos/:id", parseJSON >>> todo.update)
     route.delete("/todos/:id", todo.destroy)
+}
+
+let router = Router { route in
+    route.router("/api", v1)
+    route.router("/api", v2)
 } >>> log
 
-co {
-    let server = Server(port: 8081, responder: router) { options in
-        OpenSSL.initialize()
-        options.SSL = try? SSLServerContext(
-            certificate: "absolute path to your certificate",
-            privateKey: "absolute path to your private key"
-        )
-    }
-    server.start()
-}
+co(Server(port: 8081, responder: router) { options in
+    options.SSL = try? SSLServerContext(
+        certificate: "absolute path to your certificate",
+        privateKey: "absolute path to your private key"
+    )
+}.start())
 
 Server(port: 8080, responder: router).start()
