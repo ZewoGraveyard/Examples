5d781dbff46415f1c105dff88bb8b5dcdc011418
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-12 04:12:01 -0800

    us start in background

--- Sources/Todo/main.swift
+++ Sources/Todo/main.swift
@@ -3,20 +3,20 @@
 #else
     import Darwin.C
 #endif
+
 import CHTTPParser
 import CLibvenice
 
 import Epoch
 import OpenSSL
-import Venice
 
 OpenSSL.initialize()
 
-co(Server(port: 8081, responder: router) { options in
+Server(port: 8081, responder: router) { options in
     options.SSL = try? SSLServerContext(
-        certificate: "/absolute/path/to/csr.pem",
-        privateKey: "/absoulte/path/to/key.pem"
+        certificate: "/home/paulofaria/csr.pem",
+        privateKey: "/home/paulofaria/key.pem"
     )
-}.start())
+}.startInBackground()
 
 Server(port: 8080, responder: router).start()
