ae2765247a774479303d3ab7c4eaba78c3bc4471
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-19 13:10:56 -0800

    serve static files

--- Sources/Todo/Router.swift
+++ Sources/Todo/Router.swift
@@ -4,8 +4,8 @@ import Middleware
 import Sideburns
 
 let router = Router { route in
-    route.router("/api", APIv1)
-    route.router("/api", APIv2)
+    route.router("/api", APIv1 >>> log)
+    route.router("/api", APIv2 >>> log)
 
     route.get("/todos") { _ in
     	let templateData: TemplateData = todoResources.todos.all.map { todo in
@@ -13,4 +13,7 @@ let router = Router { route in
     	}
     	return try Response(status: .OK, templatePath: "Resources/todos.mustache", templateData: templateData)
     }
-} >>> log
+
+    let staticFiles = FileResponder(basePath: "Resources/")
+    route.fallback(staticFiles)
+} 
