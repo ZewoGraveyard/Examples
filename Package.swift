105370445b1d5c996da57e7ab61884f70be94be7
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-20 07:04:16 -0800

    fix 2015-12-18 toolchain SPM bug

--- Package.swift
+++ Package.swift
@@ -1,13 +1,16 @@
 import PackageDescription
 
 let package = Package(
-	name: "Todo",
-	dependencies: [
+    name: "Todo",
+    dependencies: [
         .Package(url: "https://github.com/Zewo/Epoch.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/Middleware.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/PostgreSQL.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/Zewo/Sideburns.git", majorVersion: 0, minor: 1)
-	]
+        .Package(url: "https://github.com/Zewo/Sideburns.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 1)
+    ]
 )
