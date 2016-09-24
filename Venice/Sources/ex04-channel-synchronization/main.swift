a5772db7c382d3592ca02ad0e74f1f55e32d985e
Romain Bossart <romain.bossart@fastmail.com>
2016-09-24 10:28:27 +0200

    [Venice] independent executables for each demo / example

---
+++ Venice/Sources/ex04-channel-synchronization/main.swift
@@ -0,0 +1,27 @@
+import Venice
+
+func ex04() {
+    // We can use channels to synchronize execution across coroutines. Here's an example of using a blocking receive to wait for a coroutine to finish.
+
+    // This is the function we'll run in a coroutine. The done channel will be used to notify another coroutine that this function's work is done.
+
+    func worker(done: Channel<Void>) {
+        print("working...")
+        nap(for: 1.second)
+        print("done")
+        done.send() // Send to notify that we're done.
+    }
+
+    // Start a worker coroutine, giving it the channel to notify on.
+
+    let done = Channel<Void>(bufferSize: 1)
+    co(worker(done: done))
+
+    // Block until we receive a notification from the worker on the channel.
+
+    done.receive()
+
+    // If you remove the done.receive() line from this program, the program would exit before the worker even started.
+}
+
+ex04()
